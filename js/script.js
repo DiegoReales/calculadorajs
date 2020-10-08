4812aa7dd529e99c3d77cbd7e83791c488e702ec
Diego Reales <diego.reales@hotmail.com>
2020-10-08 18:35:55 -0500

    Finiquitando Detalles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-let operandoa = "0";
-let operandob = "0";
-let operacion = "";
 let nuevaentrada = true;
-let finished = true;
-var clic = 0;
-
 const result = document.getElementById('result');
 const calc = document.getElementById('calc');
 const clear = document.getElementById('clear');
+const history = document.getElementById("history");
 
 calc.addEventListener('click', fcalc);
 clear.addEventListener('click', fclear);
@@ -20,60 +15,36 @@ for (const btn of numbers) {
     });
 }
 
-const symbols = document.querySelectorAll('button.symbol');
-for (const btn of symbols) {
-    btn.addEventListener('click', function (e) {
-        if (operandoa == 0 || finished) {
-            operandoa = result.textContent;
-            operacion = this.value;
-            console.log(operandoa, operacion, operandob);
-            clic=0;
-        } else {
-            operandob = result.textContent;
-            console.log(operandoa, operacion, operandob);
-            result.textContent = eval(operandoa + operacion + operandob);
-            operandoa = result.textContent;
-            operacion = this.value;
-        }
-        nuevaentrada = true;
-        finished = false;
-    });
-}
-
 function add(key) { 
     let current = result.textContent
     if ((current == "0" || nuevaentrada) && key == '.') {
         current = "0.";
+    } else if (nuevaentrada && !['+', '-', '*', '/'].includes(key)) {
+        current = key;
     } else  if (key != '.' || !current.includes('.')) {
-        current = (!nuevaentrada && (current != '0' || isNaN(key))) ? current + key : key;
+        current = (current != '0' || isNaN(key)) ? current + key : key;
     }
     result.textContent = current;
     nuevaentrada = false;
 }
 
 function fcalc() {
-    if (clic == 0) {
-        operandob = result.textContent;
-        console.log(operandoa, operacion, operandob);
-        result.textContent = eval(operandoa + operacion + operandob); 
-        document.getElementById("history").innerHTML += operandoa+operacion+operandob+"="+result.textContent+"<br/>";
-        operandoa = result.textContent;
-        nuevaentrada = true;
-        finished = true;
-        clic++;
-    } 
+    const operacion = result.textContent;
+    result.textContent = eval(operacion); 
+    if (operacion != result.textContent)
+        faddH(`${operacion} = ${result.textContent}`);
+    nuevaentrada = true;
 }
 
 function fclear() { 
-    operandoa = "0";
-    operandob = "0";
-    operacion = "";
     nuevaentrada = true;
-    finished = true;
     result.textContent = 0;
-    click=0;
 }
 
-function fclearH(){
-    document.getElementById("history").innerHTML = "";
+function faddH(s) {
+    history.innerHTML += `<li>${s}</li>`;
+}
+
+function fclearH() {
+    history.innerHTML = "";
 }
